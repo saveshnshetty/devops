5a7dea335dbbe91a44324cacdb74bbffe3bd2fc0
saveshnshetty <sarvesh@mactores.com>
2017-08-10 15:06:47 +0530

    updated puppet ip

--- userdata.ps1
+++ userdata.ps1
@@ -8,7 +8,7 @@ $client = new-object System.Net.WebClient
 $client.DownloadFile("https://downloads.puppetlabs.com/windows/puppet5/puppet-agent-5.0.0-x64.msi","c:\temp\puppet.msi")
 cd "c:\temp"
 pwd
-echo "`n13.93.159.60 puppet"  | Out-File -FilePath "C:\Windows\System32\drivers\etc\hosts" -Append -Encoding ascii
+echo "`n52.160.104.229 puppet"  | Out-File -FilePath "C:\Windows\System32\drivers\etc\hosts" -Append -Encoding ascii
 Start-Process -FilePath msiexec -ArgumentList '/qn /norestart /i "c:\temp\puppet.msi"' -Wait -NoNewWindow
 #msiexec /qn /norestart /i "c:\temp\puppet.msi" 
 if(test-path -path "C:\ProgramData\PuppetLabs\facter\facts.d")
